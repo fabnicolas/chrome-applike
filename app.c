52beab4d131063a2d208e06d773736dfebef5cf0
Finalgalaxy <finalgalaxy@gmail.com>
2017-10-15 16:13:01 +0200

    Updated app.c, now it supports both Windows and Linux systems.

--- app.c
+++ app.c
@@ -1,17 +1,33 @@
 #include <stdlib.h>		// Standard library.
 #include <stdio.h>		// Standard I/O support.
 #include <sys/stat.h>	// Chmod.
-#include <sys/wait.h>	// Wait.
 #include <unistd.h>		// Fork, execl.
 
+#ifdef linux
+#include <sys/wait.h>	// Wait (Linux-only).
+#endif
+
 int main(void){
-	char* shell_file = "./generated_linux_runtime.sh";		// Temporary name for shell to execute Chrome.
-	const char *shell_code = "#!/bin/sh\n"					// It's a shell file.
-	"google-chrome"											// The process name.
-	" --app=http://finalgalaxy.github.io/vistriker-FE/"		// Link to website to render as app.
-	" --force-app-mode"										// Force Chrome in app mode.
-	" --hide-scrollbars"									// Additional flag for app mode.
-	"&";													// Detach process when invoked.
+	char* shell_file;		// Temporary name for shell to execute Chrome.
+	const char *shell_code; // Shell batch/bash code.
+
+	#ifdef _WIN32
+		shell_file = "generated_windows_runtime.bat";			// Windows BAT file.
+		shell_code = "@echo off\n"	// Don't show what BAT executes as commands.
+		"SET URL=http://finalgalaxy.github.io/vistriker-FE/\n"	// Website link.
+		"FOR /F \"skip=2 tokens=2,*\" %%A IN ('REG QUERY \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\App Paths\\chrome.exe\" /v Path') DO SET CHROMEPATH=%%B\n"	// Try to detect from Windows Registry where C is installed and save it in CHROMEPATH variable.
+		"\"%CHROMEPATH%/chrome\" --app=%URL% --force-app-mode --hide-scrollbars";	// Execute Chrome through CHROMEPATH variable.
+	#endif
+
+	#ifdef linux
+		shell_file = "./generated_linux_runtime.sh";			// Linux BASH file.
+		shell_code = "#!/bin/sh\n"								// It's a bash shell file.
+		"google-chrome"											// The process name.
+		" --app=http://finalgalaxy.github.io/vistriker-FE/"		// Website link.
+		" --force-app-mode"										// Force Chrome in app mode.
+		" --hide-scrollbars"									// Additional flag for app mode.
+		"&";
+	#endif
 
 	FILE *f = fopen(shell_file, "w");	// Open in write mode.
 	if(f == NULL){
@@ -21,13 +37,21 @@ int main(void){
 	fprintf(f, "%s", shell_code);	// Write shell code in a temporary .sh.
 	fclose(f);
 	printf("Launching application...\n");
-	pid_t pid;
-	if((pid=fork())==0){
-		chmod(shell_file, strtol("0777",0,8));				// Make .sh executable.
-		execl("/bin/bash", "bash", shell_file, (char*)0);	// Launch .sh.
-	}
-	wait(NULL);		// Wait for child to finish...
-	printf("Application launched, terminal is closing.\n");
-	unlink(shell_file);		// Remove temporary .sh once app is started.
+
+	#ifdef _WIN32
+		system(shell_file);	// Just execute the BAT file.
+	#endif
+
+	#ifdef linux
+		pid_t pid;
+		if((pid=fork())==0){
+			chmod(shell_file, strtol("0777",0,8));				// Make .sh executable.
+			execl("/bin/bash", "bash", shell_file, (char*)0);	// Launch .sh.
+		}
+		wait(NULL);		// Wait for child to finish...
+	#endif
+
+	printf("Application launched.\n");
+	unlink(shell_file);		// Remove temporary shell file once app is started.
 	exit(0);
 }
